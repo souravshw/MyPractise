3d6eae1cba73d10719b0db56e5020563cb29bd0e
Sourav Sanu Shaw <sourav.shaw@globals.com>
2017-05-27 20:12:20 +0530

    Modified some classes

--- src/InnerClass/AnonymousInner.java
+++ src/InnerClass/AnonymousInner.java
@@ -1,23 +1,36 @@
 package InnerClass;
 //class that have no name is known as anonymous inner class in java. It should be used if you have to override method of class or interface.
-class Concrete {
-	void eat() {
-		System.out.println("Hello Motto");
-	}
 
-	void drink() {
-		System.out.println("Drink pepsi Motto");
-	}
+ abstract class Concrete {
+
+     abstract void eat();
+
+    void drink() {
+        System.out.println("Drink pepsi Motto");
+    }
+}
+
+interface Boy {
+
+    void height();
 }
 
 class AnonymousInner {
-	public static void main(String args[]) {
-		Concrete p = new Concrete() {
-			void eat() {
-				System.out.println("nice fruits");
-			}
-		};
-		p.eat();
-		p.drink();
-	}
+
+    public static void main(String args[]) {
+        Concrete p = new Concrete() {
+            void eat() {
+                System.out.println("Nice Fruits");
+            }
+        };
+        p.eat();
+        p.drink();
+
+        Boy b = new Boy() {
+            public void height() {
+                System.out.println("I am Very tall.");
+            }
+        };
+        b.height();
+    }
 }
